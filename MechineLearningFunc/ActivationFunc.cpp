193ede13275f051021fdc010e825a0f68cde8935
Pj <550392179@qq.com>
2019-03-24 23:30:23 +0800

    softmax

--- MechineLearningFunc/ActivationFunc.cpp
+++ MechineLearningFunc/ActivationFunc.cpp
@@ -11,11 +11,47 @@ ActivationFunc::~ActivationFunc()
 {
 }
 
-int ActivationFunc::softMax()
-{
 
 
-
-
-	return 1;
-}
+//template<typename _Tp>
+//int softMax_normal(const _Tp *src, const _Tp *dst, int length)
+//{
+//	const _Tp alpha= *std::max_element(src, src + length);//STL算法，找到最大元素
+//	_Tp denominator{ 0 };//定义分母
+//
+//	for (int i = 0; i<length; ++i)
+//	{
+//		dst[i] = std::exp(src[i] - alpha);// e(x+c)
+//		denominator += {dst[i]};
+//	}
+//
+//	for (int i; i<length; ++i)
+//	{
+//		dst[i] = dst[i] / denominator;
+//	}
+//
+//	return 0;
+//}
+//
+//
+//template<typename _Tp>
+//int ActivationFunc::softMax(const _Tp * src, const _Tp * dst, int length)
+//{
+//	for (int i=0;i<length;++i)
+//	{
+//	dst[i] =(_Tp)(1. / 1. + exp(src[i] + alpha));
+//	}
+//	return 0;
+//}
+//
+//
+//
+//template<typename _Tp>
+//int softMax_fast(const _Tp *src, const _Tp *dst, int length)
+//{
+//	for (int i=0;i<length;++i)
+//	{
+//		dst[i] = (_Tp)(src[i] / (1. + fabs(src[i])));
+//		return 0;
+//	}
+//}
--- MechineLearningFunc/ActivationFunc.h
+++ MechineLearningFunc/ActivationFunc.h
@@ -1,4 +1,15 @@
 #pragma once
+
+#include <math.h>
+#include <string>
+#include <iostream>	
+#include <vector>
+
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+
 class ActivationFunc
 {
 public:
@@ -10,9 +21,32 @@ public:
 
 
 public:
-	int softMax();
 
-
+	template<typename _Tp>
+	int softMax_normal( _Tp *src,  _Tp *dst,int length);
+	template<typename _Tp>
+	int softMax(const _Tp *src, const _Tp *dst, int length);
+	template<typename _Tp>
+	int softMax_fast(const _Tp *src, const _Tp *dst, int length);
 
 };
 
+template<typename _Tp>
+inline int ActivationFunc::softMax_normal( _Tp * src,  _Tp * dst, int length)
+{
+	const _Tp alpha= *std::max_element(src, src + length);//STL算法，找到最大元素
+	_Tp denominator{ 0 };//定义分母
+	
+	for (int i = 0; i<length; ++i)
+	{
+		dst[i] = std::exp(src[i] - alpha);// e(x+c)
+		denominator += dst[i];
+	}
+	
+	for (int i=0; i<length; ++i)
+	{
+		dst[i] = dst[i] / denominator;
+	}
+	
+	return 0;
+}
